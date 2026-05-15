193be624e4a97838bfbbd1a71cb61c71495b2d5c
Oliver Weiß <oliver.weiss@appsfactory.de>
2026-05-15 09:24:38 +0200

    Add ESLint setup

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
-import './style.css'
-import typescriptLogo from './assets/typescript.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import { setupCounter } from './counter.ts'
+import "./style.css";
+import typescriptLogo from "./assets/typescript.svg";
+import viteLogo from "./assets/vite.svg";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+import heroImg from "./assets/hero.png";
+import { setupCounter } from "./counter.ts";
+
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
 <section id="center">
   <div class="hero">
     <img src="${heroImg}" class="base" width="170" height="179">
@@ -55,6 +56,6 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 
 <div class="ticks"></div>
 <section id="spacer"></section>
-`
+`;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
