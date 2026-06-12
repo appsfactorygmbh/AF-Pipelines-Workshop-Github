6b66a9239551c05b2193550714ad47f4d8da7f56
Denis Moroz <denis.moroz@appsfactory.de>
2026-06-12 12:55:00 +0200

    linter issue fixed

--- scripts/postinstall.mjs
+++ scripts/postinstall.mjs
@@ -4,6 +4,7 @@ const pkg = JSON.parse(readFileSync("./package.json", "utf8"));
 
 console.log(`\n✔ postinstall: ${pkg.name}@${pkg.version} – dependencies installed successfully.`);
 
+// eslint-disable-next-line no-undef
 const nodeVersion = process.versions.node;
 const [major] = nodeVersion.split(".").map(Number);
 const REQUIRED_MAJOR = 20;
@@ -12,6 +13,7 @@ if (major < REQUIRED_MAJOR) {
   console.error(
     `✖ postinstall: Node.js >= ${REQUIRED_MAJOR} is required (found ${nodeVersion}). Please upgrade.`
   );
+  // eslint-disable-next-line no-undef
   process.exit(1);
 }
 
